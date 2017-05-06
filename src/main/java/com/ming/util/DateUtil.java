bc21cd62c8630d274e55fae71ead96046f13d4be
futao <futy911@163.com>
2017-05-06 18:45:46 +0800

    util

---
+++ src/main/java/com/ming/util/DateUtil.java
@@ -0,0 +1,27 @@
+package com.ming.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	public final static String DEFAULT = "yyyy-MM-dd HH:mm:ss";
+	public final static String DEFAULT_DATE = "yyyy-MM-dd";
+	
+	public final static String PATTERN_MIN = "yyyy-MM-dd HH:mm";
+	
+	public static Date parse(String str, String pattern) {
+		try {
+			if (str != null && str.length() > 0) {
+				SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+				return sdf.parse(str);
+			}
+		} catch (ParseException e) {
+		}
+		return null;
+	}
+	
+	public static Date parse(String str){
+		return parse(str, DEFAULT);
+	}
+}
---
+++ src/main/java/com/ming/util/JsonUtil.java
@@ -0,0 +1,15 @@
+package com.ming.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtil {
+	private static Gson gson = null;
+	static{
+		gson = new GsonBuilder().setDateFormat(DateUtil.DEFAULT).create();
+	}
+	
+	public static String toJson(Object obj){
+		return gson.toJson(obj);
+	}
+}
