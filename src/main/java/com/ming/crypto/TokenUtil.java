02da622dcb41e2fef608f8d6e3b0b1a758d0606e
zizi.liu <zizi.liu@dianping.com>
2017-05-13 14:38:37 +0800

    cry

--- src/main/java/com/ming/crypto/TokenUtil.java
+++ src/main/java/com/ming/crypto/TokenUtil.java
@@ -2,11 +2,23 @@ package com.ming.crypto;
 
 import com.ming.crypto.codec.HexString;
 
-
 public class TokenUtil {
 
 	private static final String key = "07C1F71A22153AE5";
 	private static final String iv = "55F930A817BDABFD";
+
+	/**
+	 * @param content
+	 * @param key
+	 *            16length
+	 * @param iv
+	 *            16length
+	 * @return
+	 */
+	public static String generateToken(String content, String key, String iv) {
+		byte[] encrpt = EncryptionUtil.encrpt(content, key, iv);
+		return HexString.convertByte2Hex(encrpt);
+	}
 
 	public static String generateToken(String content) {
 		byte[] encrpt = EncryptionUtil.encrpt(content, key, iv);
